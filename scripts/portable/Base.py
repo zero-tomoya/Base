0982c7ad60e8ae64f1705806a7ac72545b5b8ce4
zero-tomoya <effgrgyggf65@gmail.com>
2020-11-02 12:50:07 +0900

    Update Source Code

--- scripts/portable/Base.py
+++ scripts/portable/Base.py
@@ -5,20 +5,29 @@ import sys,pickle
 from tkinter import messagebox
 from tkinter import ttk
 import os
+import time
 import webbrowser
 import traceback
+import threading
 import tkinter.font as tkFont
 import configparser
 # this Softwere Defalut browser is keyword search 
 
+s=0
 print('')
 print(' simple Base browser　Ver 1.5')
 # 文字色の指定
+
 cor="Black"
-if os.path.isfile("./default.conf")==True:
+back="grey"
+if os.path.isfile("resource/config/default.conf")==True:
     config = configparser.ConfigParser()
-    config.read('./default.conf')
-    cor=config['SECTION1'].get('color')
+    config.read('resource/config/default.conf')
+    conf=config['SECTION1'].get('enable')
+    if conf=="True" or conf==True:
+        cor=config['SECTION1'].get('color')
+        back=config['SECTION1'].get('backcolor')
+    
 try:
 
     def resourcePath(filename):
@@ -27,9 +36,11 @@ try:
       return os.path.join(filename)
 
     def exitf(event):
+        global s
         ret=messagebox.askyesno("Simple Base browser", "このプログラムを終了しますか?")
         if ret==True:
             root.destroy()
+            s=1
             sys.exit(1)
         else:
             return "break"
@@ -37,7 +48,7 @@ try:
     def vf(event):
         global txt
         global combo
-        with open("resource/url.DAT", 'rb') as f:
+        with open("resource/dat/url.DAT", 'rb') as f:
             url = pickle.load(f)
         if txt.get()=="":
             return "break"
@@ -49,17 +60,23 @@ try:
             else:
                 web="https://"+txt.get()
         webbrowser.open_new(web)
-        txt.delete(0,tlkinter.END)
+        txt.delete(0,kinter.END)
         return "break"
-
+    
+    def dcall(event):
+        time.sleep(0.1)
+        thread1 = threading.Thread(target=vf,args=(event,))
+        thread1.start()
+        return "break"
+        
     root = tkinter.Tk()
     root.resizable(False, False)
     root['bg'] = 'grey'
-    root.iconbitmap(default="resource/ifs.ico")
+    root.iconbitmap(default="resource/icon/ifs.ico")
     Static1 = tkinter.Label(text=u' ', background='grey')
     Static4 = tkinter.Label(text=u' ', background='grey')
 
-    root.title(u"Simple Base browser")
+    root.title(u"Simple Base browser (portable)")
     root.geometry("400x350")
 
     fontStyle = tkFont.Font(family="Meiryo", size=20)
@@ -67,31 +84,31 @@ try:
     Static1.pack()
     Static1.pack()
 
-    lbl1 = tkinter.Label(text='Simple Base browser',foreground=cor, font=fontStyle,background="grey")
+    lbl1 = tkinter.Label(text='Simple Base browser',foreground=cor, font=fontStyle,background=back)
     lbl1.pack()
 
     Static2 = tkinter.Label(text=u' ',background='grey')
     Static2.pack()
 
 # ラベルの生成
-    lbl = tkinter.Label(text='検索キーワード',foreground=cor,background="grey")
+    lbl = tkinter.Label(text='検索キーワード',foreground=cor,background=back)
     lbl.pack()
     txt = tkinter.Entry(width=20)
     txt.pack()
-    Static4 = tkinter.Label(text=u' ', background='grey')
+    Static4 = tkinter.Label(text=u' ', background=back)
     Static4.pack()
 
 # 検索ボタンの生成
     Button5 = tkinter.Button(text=u'検索',font=("",10))
-    Button5.bind("<Button-1>",vf)
+    Button5.bind("<Button-1>",dcall)
     Button5.pack()
 
-    Static5 = tkinter.Label(text=u' ', background='grey')
+    Static5 = tkinter.Label(text=u' ', background=back)
     Static5.pack()
 #background
 
 #foreground
-    Static6 = tkinter.Label(text=u'モード切替',foreground=cor,background="grey")
+    Static6 = tkinter.Label(text=u'モード切替',foreground=cor,background=back)
     Static6.pack()
 
 # コンボボックスの生成
@@ -100,14 +117,15 @@ try:
     combo.current(0)
     combo.pack()
 
-    Static7 = tkinter.Label(text=u' ', background='grey')
+    Static7 = tkinter.Label(text=u' ', background=back)
     Static7.pack()
 
     Button3 = tkinter.Button(text=u'閉じる',font=("",10))
     Button3.bind("<Button-1>",exitf)
     Button3.pack()
     root.mainloop()
-
+except SystemExit:
+    pass
 except:
     with open('error.log', 'a') as fs:
         traceback.print_exc(file=fs)
